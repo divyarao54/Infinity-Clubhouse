a3a513f495079a06d1b667c35ba909a0dc5cf840
divyarao54 <divyasecondary@gmail.com>
2025-06-24 20:40:49 +0530

    first commit

--- src/components/events.js
+++ src/components/events.js
@@ -1,4 +1,7 @@
 import "./events.css";
+import birthdayEvent from "../assets/birthday-event.jpg"
+import playDate from "../assets/play-date.jpg"
+import teamBonding from "../assets/team-bonding.jpg"
 
 const Events = () =>{
     return(
@@ -9,19 +12,19 @@ const Events = () =>{
             <div className="event-group">
                 <div className="event-section">
                     <div className="event-title">Birthday Bashes!</div>
-                    <img className="event-img" src="/assets/birthday-event.jpg" alt="event-img"/>
+                    <img className="event-img" src={birthdayEvent} alt="event-img"/>
                     <div className="event-desc">Thrilling activities, exciting games, and delicious food—our custom packages ensure unforgettable experiences for all ages!</div>
                     <button className="event-btn">Join Now!</button>
                 </div>
                 <div className="event-section">
                     <div className="event-title">Club-Events!</div>
-                    <img className="event-img" src="/assets/team-bonding.jpg" alt="event-img"/>
+                    <img className="event-img" src={playDate} alt="event-img"/>
                     <div className="event-desc">Talk to us to find out more about our packages for team building activities, leadership meets, outbound learning and more.</div>
                     <button className="event-btn">Join Now!</button>
                 </div>
                 <div className="event-section">
                     <div className="event-title">Play Dates!</div>
-                    <img className="event-img" src="/assets/play-date.jpg" alt="event-img"/>
+                    <img className="event-img" src={teamBonding} alt="event-img"/>
                     <div className="event-desc">Experience Play Dates, any day of the week, enjoy this special offer and quality time with your partner. Perfect for fun and connection!</div>
                     <button className="event-btn">Join Now!</button>
                 </div>
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./hero.css";
+import heroVideo from "../assets/hero-video.mp4"
+import starIcon from "../assets/star.png"
 
 const Hero = () => {
   const [isMobileMenu, setIsMobileMenu] = useState(false);
@@ -30,7 +32,7 @@ const Hero = () => {
   return (
     <div>
       <video autoPlay loop muted playsInline className="hero-video">
-        <source src="/assets/hero-video.mp4" type="video/mp4" />
+        <source src={heroVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
@@ -47,7 +49,7 @@ const Hero = () => {
                   style={{ backgroundColor: color }}
                   onClick={() => handleStarClick(index)}
                 >
-                  <img className="star-icon" alt="star-icon" src="/assets/star.png" />
+                  <img className="star-icon" alt="star-icon" src={starIcon} />
                 </div>
               )
             )}
@@ -77,27 +79,27 @@ const Hero = () => {
       ) : (
         <div className="hero-menu">
           <div className="hero-menu-item" style={{ backgroundColor: "lightblue" }}>
-            <img className="item-star" src="/assets/star.png" alt="star-icon" />
+            <img className="item-star" src={starIcon} alt="star-icon" />
             <div className="menu-item-title">Tennis Club</div>
             <div className="menu-item-desc">Tennis Club for Tennis enthusiasts!</div>
           </div>
           <div className="hero-menu-item" style={{ backgroundColor: "lightcoral" }}>
-            <img className="item-star" src="/assets/star.png" alt="star-icon" />
+            <img className="item-star" src={starIcon} alt="star-icon" />
             <div className="menu-item-title">Cricket Club</div>
             <div className="menu-item-desc">Cricket Club for Cricket enthusiasts!</div>
           </div>
           <div className="hero-menu-item" style={{ backgroundColor: "lightgoldenrodyellow" }}>
-            <img className="item-star" src="/assets/star.png" alt="star-icon" />
+            <img className="item-star" src={starIcon} alt="star-icon" />
             <div className="menu-item-title">Football Club</div>
             <div className="menu-item-desc">Football Club for Football enthusiasts!</div>
           </div>
           <div className="hero-menu-item" style={{ backgroundColor: "lightgreen" }}>
-            <img className="item-star" src="/assets/star.png" alt="star-icon" />
+            <img className="item-star" src={starIcon} alt="star-icon" />
             <div className="menu-item-title">Chess Club</div>
             <div className="menu-item-desc">Chess Club for Chess enthusiasts!</div>
           </div>
           <div className="hero-menu-item" style={{ backgroundColor: "lightpink" }}>
-            <img className="item-star" src="/assets/star.png" alt="star-icon" />
+            <img className="item-star" src={starIcon} alt="star-icon" />
             <div className="menu-item-title">Music Club</div>
             <div className="menu-item-desc">Music Club for Music enthusiasts!</div>
           </div>
